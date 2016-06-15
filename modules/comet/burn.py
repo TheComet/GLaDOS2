8751daa7fd39ea4939f1827b8e3c091df5b40634
TheComet <alex.murray@gmx.ch>
2016-06-15 02:58:19 +0200

    Burn

--- modules/comet/burn.py
+++ modules/comet/burn.py
@@ -1,6 +1,4 @@
-from willie import module
-import codecs
-import os
+import glados
 
 burns = [
     "OOOOOOOH YOU JUST GOT BURNED, SON",
@@ -11,58 +9,38 @@ burns = [
     "#BURN #APPLYWATER #GOHOMESON",
     "NEIN CAN HEAR YOU CAUSE JEWS GOT BURNED"
 ]
-counter = 0
 
 
-def configure(config):
-    if config.option("Configure burns", False):
-        config.interactive_add("burns", "burns_data_path", "Path to where you'd like to store the burn counts between users")
+class Burn(glados.Module):
 
+    def __init__(self, settings):
+        super(Burn, self).__init__(settings)
+        self.counter = 0
+        self.burns_dict = dict()
 
-def setup(willie):
-    global burns_dict
-    global burns_data_file
-    burns_data_file = os.path.join(willie.config.burns.burns_data_path, "burns.txt")
-    
-    burns_dict = dict()
-    if os.path.isfile(burns_data_file):
-        for line in codecs.open(burns_data_file, 'r', encoding='utf-8'):
-            nick = line.split(": ")[0]
-            count = int(line.split(": ")[1])
-            burns_dict[nick] = count
+    @glados.Module.commands("burn")
+    def burn_user(self, client, message, content):
+        global burns
 
+        if content == "":
+            yield from client.send_message(message.channel, ".burn <user>")
+            return
 
-def shutdown(willie):
-    global burns_dict
-    global burns_data_file
+        user_being_burned = content.strip('@')
+        user_burning = message.author.name
 
-    with codecs.open(burns_data_file, 'a', encoding='utf-8') as f:
-        for nick, count in burns_dict.iteritems():
-            f.write(nick + ": " + count + "\n")
+        burn = burns[self.counter]
+        self.counter = (self.counter + 1) % len(burns)
 
+        if not user_burning in self.burns_dict:
+            self.burns_dict[user_burning] = dict()
+        if not user_being_burned in self.burns_dict[user_burning]:
+            self.burns_dict[user_burning] = {user_being_burned: 0}
+        if not user_being_burned in self.burns_dict:
+            self.burns_dict[user_being_burned] = {user_burning: 0}
+        self.burns_dict[user_burning][user_being_burned] += 1
 
-@module.commands("burn")
-def burn_user(bot, trigger):
-    global burns
-    global counter
-    global burns_dict
-
-    if not trigger.group(2):
-        bot.reply(".burn <user>")
-        return
-
-    user_being_burned = trigger.group(2)
-    user_burning = trigger.nick
-
-    burn = burns[counter]
-    counter = (counter + 1) % len(burns)
-
-    if not user_being_burned in burns_dict:
-        burns_dict[user_being_burned] = 0
-    if not user_burning in burns_dict:
-        burns_dict[user_burning] = 0
-    burns_dict[user_burning] += 1
-
-    bot.say(user_being_burned + ": " + burn)
-    bot.say(user_burning + ": " + str(burns_dict[user_burning]))
-    bot.say(user_being_burned + ": " + str(burns_dict[user_being_burned]))
+        response = "@{0} {1}\n{2}: {3}\n{4}: {5}".format(user_being_burned, burn,
+                                                         user_burning, self.burns_dict[user_burning][user_being_burned],
+                                                         user_being_burned, self.burns_dict[user_being_burned][user_burning])
+        yield from client.send_message(message.channel, response)
