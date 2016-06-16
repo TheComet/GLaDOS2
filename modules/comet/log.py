e0ae3af2cf5ec9ebe5450d9480187b468b28e7c0
TheComet <alex.murray@gmx.ch>
2016-06-16 20:39:56 +0200

    log file are split up into per-day logs

--- modules/comet/log.py
+++ modules/comet/log.py
@@ -8,11 +8,23 @@ class Log(glados.Module):
     def __init__(self, settings):
         super(Log, self).__init__(settings)
 
-        log_file = os.path.join(settings['modules']['config path'], 'log.txt')
-        self.__log = open(log_file, 'a')
+        self.__log_path = os.path.join(settings['modules']['config path'], 'log')
+        if not os.path.exists(self.__log_path):
+            os.makedirs(self.__log_path)
+
+        self.__date = datetime.now().strftime('%Y-%m-%d')
+        self.__log = open(os.path.join(self.__log_path, 'chanlog-{}'.format(self.__date)), 'a')
+
+    def __open_new_log_if_necessary(self):
+        date = datetime.now().strftime('%Y-%m-%d')
+        if not self.__date == date:
+            self.__log.close()
+            self.__date = date
+            self.__log = open(os.path.join(self.__log_path, 'chanlog-{}'.format(self.__date)), 'a')
 
     @glados.Module.rules('^.*$')
     def on_message(self, client, message, match):
+        self.__open_new_log_if_necessary()
         info = '[{0}] {1}: #{2}: {3}: {4}\n'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
                                                     message.server.name,
                                                     message.channel.name,
