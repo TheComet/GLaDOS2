388cbd4372de7c85bdf843aeb5340e54e14b6778
TheComet <alex.murray@gmx.ch>
2016-06-16 13:34:02 +0200

    urban dictionary works

--- modules/comet/urban.py
+++ modules/comet/urban.py
@@ -10,37 +10,25 @@ class Urban(glados.Module):
     @staticmethod
     def get_def(word):
         url = UD_URL + word
-        resp = json.loads(urllib.request.urlopen(url).read())
+        resp = json.loads(urllib.request.urlopen(url).read().decode("utf-8"))
         if resp['result_type'] == 'no_results':
             definition = 'Definition %s not found!' % (word)
         else:
             try:
-                item = resp['list'][0]['definition'].encode('utf8')
+                item = resp['list'][0]['definition']
                 thumbsup = resp['list'][0]['thumbs_up']
                 thumbsdown = resp['list'][0]['thumbs_down']
                 points = str(int(thumbsup) - int(thumbsdown))
-                total_nom = len(resp['list'])
-                definition = 'Definition: ' + str(item) + " >> Number: " + str(nom) + '/' + str(total_nom) + ' >> Points: ' + points + ' (03' + str(thumbsup) + '|05' + str(thumbsdown) + ')'
+                definition = 'Definition: ' + str(item) + ' >> Points: ' + points + ' (03' + str(thumbsup) + '|05' + str(thumbsdown) + ')'
             except IndexError:
                 definition = ('Definition entry %s does'
                               'not exist for \'%s\'.' % (1, word))
         return definition
 
-
-    @commands('urban', 'ud')
-    def urban(self, bot, trigger):
-        if not trigger.group(2):
-                bot.reply('.urban [defnum] <term>')
+    @glados.Module.commands('urban', 'ud')
+    def urban(self, client, message, content):
+        if content == '':
+                yield from client.send_message(message.channel, '.urban <term>')
                 return
-        args = trigger.group(2).replace(', ', ',').split(',')
-        defnum = 0
-        word = ' '.join(args)
-        if len(args) > 1:
-            try:
-                defnum = int(args[0])
-                defnum = defnum - 1
-                word = ' '.join(args[1:])
-            except ValueError:
-                pass
-        definition = get_def(word, defnum).replace('\r', ' / ')
-        bot.say(definition, max_messages=5)
+        definition = self.get_def(content)
+        yield from client.send_message(message.channel, definition)
