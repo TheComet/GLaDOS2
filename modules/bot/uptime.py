01131b6f83650d98c3fcd71128f11226bbcf2cd2
TheComet <alex.murray@gmx.ch>
2016-06-21 07:06:59 +0200

    Adding more messages to .uptime

--- modules/bot/uptime.py
+++ modules/bot/uptime.py
@@ -8,8 +8,18 @@ http://sopel.chat
 import glados
 from datetime import datetime
 from datetime import timedelta
+import random
+
 
 class UpTime(glados.Module):
+
+    messages = (
+        "I've been sitting here for {} and I keep going!",
+        "I'm still sitting here. It's been {} and I try!",
+        "It's been so long, I can't even tell you. I think {}",
+        "Sitting is a thing humans do. I actually just exist. I've been existing for {}",
+        "I am but an abstract description being used in an abstract way. Been so for {}"
+    )
 
     def __init__(self, settings):
         super().__init__(settings)
@@ -23,4 +33,4 @@ class UpTime(glados.Module):
     @glados.Module.commands('uptime')
     def uptime(self, client, message, arg):
         delta = timedelta(seconds=round((datetime.utcnow() - self.__started).total_seconds()))
-        yield from client.send_message(message.channel, "I've been sitting here for {} and I keep going!".format(delta))
+        yield from client.send_message(message.channel, random.choice(self.messages).format(delta))
