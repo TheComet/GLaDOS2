86ec3c4d8dee097a20a41b175e96e9c8ca1ec7e7
TheComet <alex.murray@gmx.ch>
2016-06-24 19:41:12 +0200

    .mentions to another module

---
+++ modules/general/mentions.py
@@ -0,0 +1,140 @@
+import glados
+import os.path
+import codecs
+import json
+from datetime import datetime
+import dateutil.parser
+
+
+def strip_timestamp_and_name(msg):
+    try:
+        dateutil.parser.parse(msg[:19])
+    except TypeError:
+        return msg
+    except ValueError:
+        return msg
+
+    return msg[19:].split(':', 1)[1].strip()
+
+
+def get_timestamp(msg):
+    try:
+        stamp = dateutil.parser.parse(msg[:19])
+    except TypeError:
+        return None
+    except ValueError:
+        return None
+
+    return stamp
+
+
+def get_author(msg):
+    try:
+        dateutil.parser.parse(msg[:19])
+    except TypeError:
+        return None
+    except ValueError:
+        return None
+
+    return msg[19:].split(':', 1)[0]
+
+
+class Mentions(glados.Module):
+    def __init__(self, settings):
+        super().__init__(settings)
+
+        self.__mention_log_file = os.path.join(settings['modules']['config path'], 'quotes', 'mentions.txt')
+
+        self.__seen_file = os.path.join(settings['modules']['config path'], 'quotes', 'seen_timestamps.json')
+        self.__last_seen = dict()
+        self.__load_seen_timestamps()
+
+    def get_help_list(self):
+        return [
+            glados.Help('mentions', '[num]', 'Returns all messages in which you were mentioned since you were last seen.'
+                                             ' If you provide a number, it will return the last [num] messages instead.')
+        ]
+
+    def __load_seen_timestamps(self):
+        glados.log('loading seen timestamps for .mentions')
+        if os.path.isfile(self.__seen_file):
+            self.__last_seen = json.loads(open(self.__seen_file).read())
+
+    def __save_seen_timestamps(self):
+        glados.log('saving seen timestamps for .mentions')
+        with open(self.__seen_file, 'w') as f:
+            f.write(json.dumps(self.__last_seen))
+
+    @glados.Module.rules('^((?!\.\w+).*)$')
+    def record(self, client, message, match):
+        with codecs.open(self.__mention_log_file, 'a', encoding='utf-8') as f:
+            f.write(datetime.now().isoformat()[:19] + "  " + message.author.name + ": " + message.clean_content + "\n")
+
+        author = message.author.name
+        key = author.lower()
+        self.__last_seen[key] = datetime.now().isoformat()[:19]  # don't need microseconds
+        self.__save_seen_timestamps()
+
+        return tuple()
+
+    @glados.Module.commands('mentions')
+    def on_seen(self, client, message, arg):
+        glados.log('Looking for mentions for author {}'.format(message.author.name))
+
+        try:
+            num = int(arg)
+        except ValueError:
+            num = 0
+
+        max_num = 5
+        mentions = list()
+        author = message.author.name
+        key = author.lower()
+
+        if num > max_num:
+            yield from client.send_message(message.channel, 'Please, don\'t be an idiot')
+            return
+
+        mentions_file = codecs.open(self.__mention_log_file, 'r', encoding='utf-8')
+        lines = mentions_file.readlines()
+        mentions_file.close()
+
+        if num == 0:
+
+            if key not in self.__last_seen:
+                yield from client.send_message(message.channel, '{0} has never been mentioned.'.format(author))
+                return
+
+            last_seen = dateutil.parser.parse(self.__last_seen[key])
+            for msg in reversed(lines):
+                stamp = get_timestamp(msg)
+                if stamp is None:
+                    continue
+                if stamp < last_seen:  # no more new messages
+                    break
+                if key in strip_timestamp_and_name(msg).lower():
+                    mentions.append(msg)
+        else:
+
+            for msg in reversed(lines):
+                if key in strip_timestamp_and_name(msg).lower():
+                    mentions.append(msg)
+                    num -= 1
+                if num == 0:
+                    break
+
+        if len(mentions) == 0:
+            yield from client.send_message(message.channel, 'No one mentioned you.')
+            return
+
+        response = list()
+        for msg in mentions:
+            author = get_author(msg)
+            if author is None:
+                author = ''
+            else:
+                author = '**' + author + '**: '
+
+            response.append(author + strip_timestamp_and_name(msg))
+
+        yield from client.send_message(message.channel, '\n'.join(response))
--- modules/general/quotes.py
+++ modules/general/quotes.py
@@ -2,42 +2,6 @@ import glados
 import codecs
 import os.path
 import random
-import json
-from datetime import datetime
-import dateutil.parser
-
-
-def strip_timestamp_and_name(msg):
-    try:
-        dateutil.parser.parse(msg[:19])
-    except TypeError:
-        return msg
-    except ValueError:
-        return msg
-
-    return msg[19:].split(':', 1)[1].strip()
-
-
-def get_timestamp(msg):
-    try:
-        stamp = dateutil.parser.parse(msg[:19])
-    except TypeError:
-        return None
-    except ValueError:
-        return None
-
-    return stamp
-
-
-def get_author(msg):
-    try:
-        dateutil.parser.parse(msg[:19])
-    except TypeError:
-        return None
-    except ValueError:
-        return None
-
-    return msg[19:].split(':', 1)[0]
 
 
 class Quotes(glados.Module):
@@ -48,19 +12,11 @@ class Quotes(glados.Module):
         if not os.path.exists(self.quotes_data_path):
             os.makedirs(self.quotes_data_path)
 
-        self.__mention_log_file = os.path.join(settings['modules']['config path'], 'quotes', 'mentions.txt')
-
-        self.__seen_file = os.path.join(settings['modules']['config path'], 'quotes', 'seen_timestamps.json')
-        self.__last_seen = dict()
-        self.__load_seen_timestamps()
-
     def get_help_list(self):
         return [
             glados.Help('quote', '<user>', 'Dig up a quote the user once said in the past.'),
             glados.Help('quotestats', '<user>', 'Provide statistics on how many quotes a user has and how'
-                                                ' intelligent he is'),
-            glados.Help('mentions', '[num]', 'Returns all messages in which you were mentioned since you were last seen.'
-                                             ' If you provide a number, it will return the last [num] messages instead.')
+                                                ' intelligent he is')
         ]
 
     def check_nickname_valid(self, author):
@@ -80,15 +36,7 @@ class Quotes(glados.Module):
     def quotes_file_name(self, author):
         return os.path.join(self.quotes_data_path, author) + '.txt'
 
-    def __load_seen_timestamps(self):
-        glados.log('loading seen timestamps for .mentions')
-        if os.path.isfile(self.__seen_file):
-            self.__last_seen = json.loads(open(self.__seen_file).read())
 
-    def __save_seen_timestamps(self):
-        glados.log('saving seen timestamps for .mentions')
-        with open(self.__seen_file, 'w') as f:
-            f.write(json.dumps(self.__last_seen))
 
     # matches everything except strings beginning with a ".xxx" to ignore commands
     @glados.Module.rules('^((?!\.\w+).*)$')
@@ -98,13 +46,6 @@ class Quotes(glados.Module):
         author = message.author.name
         with codecs.open(self.quotes_file_name(author.lower()), 'a', encoding='utf-8') as f:
             f.write(match.group(1))
-
-        with codecs.open(self.__mention_log_file, 'a', encoding='utf-8') as f:
-            f.write(datetime.now().isoformat()[:19] + "  " + message.author.name + ": " + message.clean_content + "\n")
-
-        key = author.lower()
-        self.__last_seen[key] = datetime.now().isoformat()[:19]  # don't need microseconds
-        self.__save_seen_timestamps()
 
         return tuple()
 
@@ -163,65 +104,3 @@ class Quotes(glados.Module):
             average_quote_length,
             author, number_of_words, average_word_length)
         yield from client.send_message(message.channel, response)
-
-    @glados.Module.commands('mentions')
-    def on_seen(self, client, message, arg):
-        glados.log('Looking for mentions for author {}'.format(message.author.name))
-
-        try:
-            num = int(arg)
-        except ValueError:
-            num = 0
-
-        max_num = 5
-        mentions = list()
-        author = message.author.name
-        key = author.lower()
-
-        if num > max_num:
-            yield from client.send_message(message.channel, 'Please, don\'t be an idiot')
-            return
-
-        mentions_file = codecs.open(self.__mention_log_file, 'r', encoding='utf-8')
-        lines = mentions_file.readlines()
-        mentions_file.close()
-
-        if num == 0:
-
-            if key not in self.__last_seen:
-                yield from client.send_message(message.channel, '{0} has never been mentioned.'.format(author))
-                return
-
-            last_seen = dateutil.parser.parse(self.__last_seen[key])
-            for msg in reversed(lines):
-                stamp = get_timestamp(msg)
-                if stamp is None:
-                    continue
-                if stamp < last_seen:  # no more new messages
-                    break
-                if key in strip_timestamp_and_name(msg).lower():
-                    mentions.append(msg)
-        else:
-
-            for msg in reversed(lines):
-                if key in strip_timestamp_and_name(msg).lower():
-                    mentions.append(msg)
-                    num -= 1
-                if num == 0:
-                    break
-
-        if len(mentions) == 0:
-            yield from client.send_message(message.channel, 'No one mentioned you.')
-            return
-
-        response = list()
-        for msg in mentions:
-            author = get_author(msg)
-            if author is None:
-                author = ''
-            else:
-                author = '**' + author + '**: '
-
-            response.append(author + strip_timestamp_and_name(msg))
-
-        yield from client.send_message(message.channel, '\n'.join(response))
