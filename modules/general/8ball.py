df8bc1e2dabedc0d12be0520479a43a0b0f4237f
TheComet <alex.murray@gmx.ch>
2016-10-16 13:23:50 +0200

    Balancing 8 ball answers

--- modules/general/8ball.py
+++ modules/general/8ball.py
@@ -18,16 +18,16 @@ messages = ["It is certain",
             "Outlook good",
             "Yes",
             "Signs point to yes",
-            "Reply hazy try again",
-            "Ask again later",
-            "Better not tell you now",
-            "Cannot predict now",
-            "Concentrate and ask again",
             "Don't count on it",
             "My reply is no",
             "God says no",
             "Very doubtful",
-            "Outlook not so good"]
+            "Outlook not so good",
+            "r u srs?",
+            "lol yeah right. In your dreams maybe",
+            "No.",
+            "Definitely not.",
+            "Maybe in a future life"]
 
 
 class EightBall(glados.Module):
