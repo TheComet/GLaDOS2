ce448912beaae12f36939c7dd68fe628d7c0b176
TheComet <alex.murray@gmx.ch>
2016-10-16 13:30:33 +0200

    Fixing shit

--- modules/gamedev/saatchi.py
+++ modules/gamedev/saatchi.py
@@ -1,9 +1,17 @@
 import glados
 
 
+responses = [
+    'SHUT UP',
+    'SHUT IT',
+    'オフファック'
+]
+
+
 class Saatchi(glados.Module):
     def get_help_list(self): return list()
 
-    @glados.Module.rules('^Boku Saatchi\!$')
+    @glados.Module.rules('^Boku Saatchi.*$')
     def saatchi(self, message, match):
-        yield from self.client.send_message(message.channel, 'SHUT UP')
+        if message.author.name == 'Saatchi':
+            yield from self.client.send_message(message.channel, 'SHUT UP')
