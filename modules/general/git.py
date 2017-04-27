7000eb7539068ec4bb95ed2a858b0c3c6e5924c0
TheComet <alex.murray@gmx.ch>
2017-04-27 22:57:00 +0200

    Adding horrible hacked github webhook shit

--- modules/general/git.py
+++ modules/general/git.py
@@ -2,6 +2,7 @@ import glados
 import json
 import asyncio
 import subprocess
+import traceback
 
 
 class PushNotifier(glados.Module):
@@ -18,20 +19,33 @@ class PushNotifier(glados.Module):
 
     @asyncio.coroutine
     def run(self):
-        ps = subprocess.Popen(('env/bin/python', 'listener.py'), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        ps = subprocess.Popen(('/home/cometbot/discord/GLaDOS2/github_webhook.py',), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        print('started server')
         while True:
             loop = asyncio.get_event_loop()
-            msg = yield from loop.run_in_executor(None, ps.communicate)
-            msg = msg[0]
+            msg = yield from loop.run_in_executor(None, ps.stdout.readline)
+            msg = msg.decode('utf-8')
+            fixed_json = msg.split('127.0.0.1')[0]
+            #fixed_json = broken_json.replace('"', '__tmp__').replace('"', "'").replace('__tmp__', "'").replace(': ,', ': "",').replace('True', '"True"').replace('False', '"False"').replace('None', '"None"')
+            print('got message {}'.format(fixed_json))
             try:
-                data = json.loads(msg)
+                data = json.loads(fixed_json)
             except:
+                tb = traceback.format_exc()
+                print(tb)
                 continue
 
-            author = data['commits'][0]['author']['name']
-            message = data['commits'][0]['message']
+            try:
+                author = data['commits'][0]['author']['username']
+                message = data['commits'][0]['message']
+            except KeyError:
+                author = data['commits'][0]['author']['name'] + ' the Rulebreaker'
+                message = data['commits'][0]['message'] + ', but I fucked it up'
             repo = data['repository']['name']
-            msg = '"{}" pushed to {}: {}'.format(author, repo, message)
+            #added = data['commits'][0]['added']
+            #removed = data['commits'][0]['removed']
+            msg = '{} pushed to {}: "{}".'.format(author, repo, message)
             for channel in self.client.get_all_channels():
                 if channel.id in self.__channels:
                     yield from self.client.send_message(channel, msg)
+
