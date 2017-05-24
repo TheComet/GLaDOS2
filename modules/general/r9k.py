dcac35c66c0d41e16534a3595cb867554a730c9b
TheComet <alex.murray@gmx.ch>
2017-05-24 09:45:38 +0200

    passive r9k with score board

--- modules/general/r9k.py
+++ modules/general/r9k.py
@@ -1,8 +1,9 @@
 import glados
 import os
-import enchant
 import hashlib
 import re
+import json
+import asyncio
 
 
 class R9K(glados.Module):
@@ -17,53 +18,64 @@ class R9K(glados.Module):
         for channel_id in self.settings['r9k']['channels']:
             memory['channels'].add(channel_id)
 
-        db_file = os.path.join(self.get_config_dir(), 'r9k.txt')
-        memory['db file'] = open(db_file, 'a')
-        memory['db'] = set()
-        for line in open(db_file):
-            memory['db'].add(line.strip())
+        # directory where r9k stuff is stored
+        memory['path'] = os.path.join(self.get_config_dir(), 'r9k')
+        if not os.path.exists(memory['path']):
+            os.makedirs(memory['path'])
 
-        memory['dictionaries'] = [
-            enchant.Dict('en_US'),
-            enchant.Dict('en_GB')
-        ]
+        # load score board, if it exists
+        memory['scores file'] = os.path.join(memory['path'], 'scores.json')
+        if os.path.isfile(memory['scores file']):
+            memory['scores'] = json.loads(open(memory['scores file']).read())
+        else:
+            memory['scores'] = dict()
+
+        # set up hash tables
+        db_file = os.path.join(memory['path'], 'hashes.txt')
+        memory['hashes file'] = open(db_file, 'a')
+        memory['hashes'] = set()
+        for line in open(db_file):
+            memory['hashes'].add(line.strip())
 
     @glados.Module.commands('r9k')
-    def toggle(self, message, args):
-        # Must be an admin
-        if message.author.id not in self.settings['admins']['IDs']:
-            yield from self.client.send_message(message.channel, 'You must be an admin to run this command')
-            return
-
+    def send_scores(self, message, args):
         memory = self.get_memory()
-        if message.channel.id not in memory['channels']:
-            memory['channels'].add(message.channel.id)
-            enabled = True
-        else:
-            memory['channels'].remove(message.channel.id)
-            enabled = False
+        msg = '**Top 5 most unoriginal users**\n'
+        top5 = sorted(memory['scores'].items(), key=lambda kv: kv[1]['score'])[:5]
+        for author, d in top5:
+            msg += '  + {} ({})'.format(author, d['score'])
 
-        msg = ('disabled', 'enabled')
-        yield from self.client.send_message(message.channel, 'ROBOT9000 {}.'.format(msg[enabled]))
+        yield from self.client.send_message(message.channel, msg)
 
     # matches everything except strings beginning with a ".xxx" to ignore commands
     @glados.Module.rules('^((?!\.\w+).*)$')
     def on_message(self, message, match):
         memory = self.get_memory()
 
-        # Remove anything that is not [a-z][A-Z]_
+        # Remove anything that is not alphanumeric
         phrase = match.group(1)
         phrase = re.sub('[^A-Za-z0-9]+', '', phrase)
         h = hashlib.sha256(phrase.encode('utf-8')).hexdigest()
 
         # Check for originality
-        if message.channel.id in memory['channels'] and h in memory['db']:
-            phrase = match.group(1)
-            if len(phrase) > 40:
-                phrase = phrase[:40] + '...'
-            yield from self.client.send_message(message.channel, '[r9k] The phrase `{}` is unoriginal!'.format(phrase))
-            return
+        if h in memory['hashes']:
+            # annoy user, if enabled
+            if message.channel.id in memory['channels']:
+                phrase = match.group(1)
+                if len(phrase) > 40:
+                    phrase = phrase[:40] + '...'
+                yield from self.client.send_message(message.channel, '[r9k] The phrase `{}` is unoriginal!'.format(phrase))
 
-        memory['db'].add(h)
-        memory['db file'].write(h + '\n')
+            # update scores
+            author = message.author.name
+            if author not in memory['scores']:
+                memory['scores'][author] = {'score': 0}
+            memory['scores'][author]['score'] += 1
+
+            with open(memory['scores file'], 'w') as f:
+                f.write(json.dumps(memory['scores']))
+
+        memory['hashes'].add(h)
+        memory['hashes file'].write(h + '\n')
+
         return tuple()
