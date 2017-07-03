a4a1b1f283133f2df93bed48b5d036e29bfefcdc
TheComet <alex.murray@gmx.ch>
2017-07-03 19:11:04 +0200

    Source command

---
+++ modules/general/source.py
@@ -0,0 +1,10 @@
+import glados
+
+class Source(glados.Module):
+    def get_help_list(self):
+        return [glados.Help('source', '', 'Returns a link to the source code of GLaDOS')
+    
+    @glados.Module.commands('source')
+    def source(self, message, args):
+        yield from self.client.send_message(message.channel, 'https://github.com/TheComet93/GLaDOS2')
+
