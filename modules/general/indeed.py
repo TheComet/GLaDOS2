4c38e3f0bedf485f9f1d495ad0fbe9fd4d5fb81a
TheComet <alex.murray@gmx.ch>
2017-07-05 18:50:30 +0200

    Adding indeed

---
+++ modules/general/indeed.py
@@ -0,0 +1,10 @@
+import glados
+import os
+
+class Indeed(glados.Module):
+    def get_help_list(self):
+        return [glados.Help('indeed', '', 'Post the indeed picture Alpheus likes so much')]
+    @glados.Module.commands('indeed')
+    def indeed(self, message, args):
+        yield from self.client.send_file(message.channel, os.path.join(os.path.dirname(os.path.realpath(__file__)), 'indeed.jpg'))
+
--- modules/general/source.py
+++ modules/general/source.py
@@ -2,7 +2,7 @@ import glados
 
 class Source(glados.Module):
     def get_help_list(self):
-        return [glados.Help('source', '', 'Returns a link to the source code of GLaDOS')
+        return [glados.Help('source', '', 'Returns a link to the source code of GLaDOS')]
     
     @glados.Module.commands('source')
     def source(self, message, args):
