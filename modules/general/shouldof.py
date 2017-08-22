40ea2124209302002c05ee86eca26306b7252798
TheComet <alex.murray@gmx.ch>
2017-08-23 00:17:11 +0200

    Fixing would of again

--- modules/general/shouldof.py
+++ modules/general/shouldof.py
@@ -3,6 +3,6 @@ import glados
 class ShouldOf(glados.Module):
     def get_help_list(self):
         return list()
-    @glados.Module.rules('^.*(?i)(sh|c|w)ould of\b.*$')
+    @glados.Module.rules('^.*(?i)(sh|c|w)ould of\\b.*$')
     def shouldof(self, message, match):
         yield from self.client.send_message(message.channel, '{}ould have*'.format(match.group(1)))
