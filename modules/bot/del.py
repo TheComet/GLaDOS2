1d4f2ad7bd45cd2b18d4afed3509aa5425eeff16
TheComet <alex.murray@gmx.ch>
2017-09-27 05:30:04 +0200

    Bot can delete its most recent messages

---
+++ modules/bot/del.py
@@ -0,0 +1,11 @@
+import glados
+
+
+class Del(glados.Module):
+    @glados.Module.command('del', '', 'Deletes the most recent bot message')
+    async def del_most_recent(self, message, content):
+        for message in self.client.messages:
+            if message.author.id == self.client.user.id:
+                await self.client.delete_message(message)
+                return
+        return ()
