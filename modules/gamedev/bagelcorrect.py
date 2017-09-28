1e27caf180bd59ab499d77b733e87ed2bc17e52a
TheComet <alex.murray@gmx.ch>
2017-09-28 23:37:10 +0200

    oops

--- modules/gamedev/bagelcorrect.py
+++ modules/gamedev/bagelcorrect.py
@@ -9,12 +9,12 @@ users_to_correct = [
 class BagelCorrect(glados.Module):
     @glados.Module.rule('^.*(?i)\\btho\\b.*$')
     async def tho(self, message, match):
-        if message.id in users_to_correct:
+        if message.author.id in users_to_correct:
             await self.client.send_message(message.channel, 'though*')
         return ()
 
     @glados.Module.rule('^.*(!i)\\bcuz\\b.*$')
     async def cuz(self, message, match):
-        if message.id in users_to_correct:
+        if message.author.id in users_to_correct:
             await self.client.send_message(message.channel, 'because*')
         return ()
