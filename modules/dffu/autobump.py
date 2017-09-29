ce8ab325f0e3f317aaa25fcdba7698f0d4ef6ee8
TheComet <alex.murray@gmx.ch>
2017-09-30 00:37:55 +0200

    T'was a bad idea

--- modules/dffu/autobump.py
+++ modules/dffu/autobump.py
@@ -1,8 +1,9 @@
 import glados
+import asyncio
 
 
 class AutoBump(glados.Module):
     @glados.Module.bot_rule('^.*Don\'t forget to bump the server.*$')
     async def do_bump(self, message, content):
-        for i in range(5):  # Are they really this retarded?
-            await self.client.send_message(message.channel, '=bump')
+        await asyncio.sleep(1)
+        await self.client.send_message(message.channel, '=bump')
