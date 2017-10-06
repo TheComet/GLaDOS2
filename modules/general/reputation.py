4b5755699f159ccfa83c9b8ed2f51c1bd47c356c
Avalander <darth.quesorico@gmail.com>
2017-10-06 13:43:43 +0200

    Make storage work

--- modules/general/reputation.py
+++ modules/general/reputation.py
@@ -12,12 +12,14 @@ class Reputation(glados.Module):
             os.makedirs(self.memory['reputation path'])
     
     def _update_reputation(self, data):
-        rep_file = os.path.join(self.memory['reputation path'], 'repuation.json')
+        rep_file = os.path.join(self.memory['reputation path'], 'reputation.json')
         with codecs.open(rep_file, 'w', encoding='utf-8') as f:
             json.dump(data, f)
     
     def _get_reputation(self):
-        rep_file = os.path.join(self.memory['reputation path'], 'repuation.json')
+        rep_file = os.path.join(self.memory['reputation path'], 'reputation.json')
+        if not os.path.exists(rep_file):
+            return {}
         with codecs.open(rep_file, 'r', encoding='utf-8') as f:
             return json.load(f)
 
@@ -33,8 +35,8 @@ class Reputation(glados.Module):
         response = []
         reputation = self._get_reputation()
         for member in members:
-            new_reputation = reputation.get(member, 0) + 1
-            reputation[member] = new_reputation
+            new_reputation = reputation.get(member.name, 0) + 1
+            reputation[member.name] = new_reputation
             response.append('{}\'s reputation is {}'.format(member.name, new_reputation))
         self._update_reputation(reputation)
         await self.client.send_message(message.channel, ', '.join(response))
@@ -48,8 +50,8 @@ class Reputation(glados.Module):
         response = []
         reputation = self._get_reputation()
         for member in members:
-            new_reputation = reputation.get(member, 0) - 1
-            reputation[member] = new_reputation
+            new_reputation = reputation.get(member.name, 0) - 1
+            reputation[member.name] = new_reputation
             response.append('{}\'s reputation is {}'.format(member.name, new_reputation))
         self._update_reputation(reputation)
         await self.client.send_message(message.channel, ', '.join(response))
@@ -61,5 +63,5 @@ class Reputation(glados.Module):
             await self.client.send_message(message.channel, error)
             return
         reputation = self._get_reputation()
-        response = ['{}\'s reputation is {}'.format(member.name, reputation.get(member, 0)) for member in members ]
+        response = ['{}\'s reputation is {}'.format(member.name, reputation.get(member.name, 0)) for member in members ]
         await self.client.send_message(message.channel, ', '.join(response))
