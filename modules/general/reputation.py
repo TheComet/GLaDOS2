88f26740ad761bc461bc62e2cebb1188f4d25bba
Avalander <darth.quesorico@gmail.com>
2017-10-06 18:47:30 +0200

    Create reputation folder if it does not exist.

--- modules/general/reputation.py
+++ modules/general/reputation.py
@@ -44,6 +44,7 @@ class Reputation(glados.Module):
             'comebacks': comebacks_file,
             'config': config_file,
         }
+        os.makedirs(os.path.join(self.data_dir, 'reputation'))
         if not os.path.exists(rep_file):
             with codecs.open(rep_file, 'w', encoding='utf-8') as f:
                 json.dump({}, f)
