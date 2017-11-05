fa964a31310bdc7291280215ae5d3700589dc857
TheComet <alex.murray@gmx.ch>
2017-11-05 03:43:24 +0100

    Fixing 'damn straight'

--- modules/general/damn_straight.py
+++ modules/general/damn_straight.py
@@ -4,4 +4,4 @@ import glados
 class DamnStraight(glados.Module):
     @glados.Module.rule("^.*(?=.*damn)(?=.*straight).*$")
     async def damn_straight(self, message, content):
-        await self.client.send_message('{} Straighter than the pole your mom dances on?', message.author.mention)
+        await self.client.send_message(message.channel, '{} Straighter than the pole your mom dances on?'.format(message.author.mention))
