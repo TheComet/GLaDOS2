7937dc403a04b9f2c4359142aac25e29c4c1be61
TheComet <alex.murray@gmx.ch>
2017-12-02 01:29:37 +0100

    Trying to add .circuit command, failing

--- modules/general/latex.py
+++ modules/general/latex.py
@@ -20,6 +20,18 @@ LATEX_FRAMEWORK = r"""
 \usepackage{mathrsfs}
 \usepackage{mathtools}
 \usepackage{skull}
+\usepackage{siunitx}
+\usepackage[american,europeanvoltages]{circuitikz}
+
+% Required for tikz plots
+\usepackage{pgfplots}
+\pgfplotsset{compat=newest}
+\usetikzlibrary{plotmarks}
+\usetikzlibrary{arrows.meta}
+\usepgfplotslibrary{patchplots}
+\usepackage{grffile}
+
+\sisetup{output-exponent-marker=\ensuremath{\mathrm{E}}}
 
 \DeclarePairedDelimiter\ceil{\lceil}{\rceil}
 \DeclarePairedDelimiter\floor{\lfloor}{\rfloor}
@@ -37,7 +49,9 @@ LATEX_FRAMEWORK = r"""
 \def\norm{\@ifstar{\oldnorm}{\oldnorm*}}
 \makeatother
 
-\newcommand{\knochen}[0]{\fourier}
+%\newcommand{\knochen}[0]{\fourier}
+\renewcommand{\L}[1]{\mathscr{L}\left\{#1\right\}}
+\newcommand{\F}[1]{\mathscr{F}\left\{#1\right\}}
 
 \newcommand{\rz}[1]{
     \begin{pmatrix}
@@ -63,12 +77,21 @@ LATEX_FRAMEWORK = r"""
 
 \begin{document}
 {\color{Black}
-\begin{align*}
-__DATA__
-\end{align*}}
+__DATA__}
 \end{document}
 """
 
+MATH_ENV = r"""
+\begin{align*}
+__DATA__
+\end{align*}
+"""
+
+CIRCUIT_ENV = r"""
+\begin{circuitikz}
+__DATA__
+\end{circuitikz}
+"""
 
 class LaTeX(glados.Module):
 
@@ -83,8 +106,16 @@ class LaTeX(glados.Module):
                                   "\\repeat",
                                   "\\csname",
                                   "\\endcsname")
+        self.__last_messages = dict()  # map username to message objects
 
-    def generate_image(self, latex):
+    @staticmethod
+    def math(latex_code):
+        return LATEX_FRAMEWORK.replace('__DATA__', MATH_ENV).replace('__DATA__', latex_code)
+    @staticmethod
+    def circuit(latex_code):
+        return LATEX_FRAMEWORK.replace('__DATA__', CIRCUIT_ENV).replace('__DATA__', latex_code)
+
+    def generate_image(self, latex, gen_func):
 
         num = str(random.randint(0, 2 ** 31))
         latex_file = os.path.join(self.__out_folder, num + '.tex')
@@ -100,7 +131,7 @@ class LaTeX(glados.Module):
         dvipng_cmd = ['dvipng', '-q*', '-D', '200', '-T', 'tight', '-bg', 'Transparent', '-o', png_file, dvi_file]
 
         with open(latex_file, 'w') as tex:
-            latex = LATEX_FRAMEWORK.replace('__DATA__', latex)
+            latex = gen_func(latex)
             tex.write(latex)
             tex.flush()
             tex.close()
@@ -123,13 +154,8 @@ class LaTeX(glados.Module):
 
         return True, png_file
 
-    @glados.Module.command('math', '<latex code>', 'Render latex math code')
-    async def on_message(self, message, content):
-
-        if content == '':
-            await self.provide_help('math', message)
-            return
-
+    @glados.Module.command('math', '<latex code>', 'Render latex math code. The code you provide is placed between \\begin{align} and \\end{align}.')
+    async def math_cmd(self, message, content):
         if any(x in content for x in self.__latex_blacklist):
             glados.log('LaTeX: Blacklisted commands detected!\n'
                        '  Author: {0}\n  Message: {1}\n  Server: {2}\n  Channel: {3}'.format(
@@ -141,19 +167,68 @@ class LaTeX(glados.Module):
             await self.client.send_message(message.channel, "Error: Trying to be naughty, are we?")
             return
 
-        # troll dsm
-        #if message.author.name == "dsm":
-        #    content = "!tex \\color{pink} \\text {dsm loves dongers}"
+        # check if this is an edited message
+        if message.edited_timestamp:
+            last_msg = self.__last_messages.pop(message.author.id, None)
+            if last_msg is not None and last_msg[0].id == message.id:
+                await self.client.delete_message(last_msg[1])
 
-        fn = self.generate_image(content)
+        # troll Cul
+        if message.author.id == '113128686969958400' and random.random() < 0.02:
+            content = "\\color{pink} \\text{CulDeVu  loves dongers}"
+
+        fn = self.generate_image(content, self.math)
 
         if not fn[0]:
             await self.client.send_message(message.channel, fn[1])
             return
         if os.path.getsize(fn[1]) > 0:
             try:
-                await self.client.send_file(message.channel, fn[1])
+                self.__last_messages[message.author.id] = (
+                    message,
+                    await self.client.send_file(message.channel, fn[1])
+                )
             except discord.errors.Forbidden:
                 error = 'Error: Insufficient permissions to send file files to channel #{}'.format(message.channel.name)
                 glados.log(error)
                 await self.client.send_message(message.channel, error)
+
+    @glados.Module.command('circuit', '<latex code>', 'Render latex math code. The code you provide is placed between \\begin{align} and \\end{align}.')
+    async def circuit_cmd(self, message, content):
+        if any(x in content for x in self.__latex_blacklist):
+            glados.log('LaTeX: Blacklisted commands detected!\n'
+                       '  Author: {0}\n  Message: {1}\n  Server: {2}\n  Channel: {3}'.format(
+                message.author.name,
+                message.content,
+                message.server.name,
+                message.channel.name
+            ))
+            await self.client.send_message(message.channel, "Error: Trying to be naughty, are we?")
+            return
+
+        # check if this is an edited message
+        if message.edited_timestamp:
+            last_msg = self.__last_messages.pop(message.author.id, None)
+            if last_msg is not None and last_msg[0].id == message.id:
+                await self.client.delete_message(last_msg[1])
+
+        # troll Cul
+        if message.author.id == '113128686969958400' and random.random() < 0.02:
+            content = "\\color{pink} \\text{CulDeVu  loves dongers}"
+
+        fn = self.generate_image(content, self.circuit)
+
+        if not fn[0]:
+            await self.client.send_message(message.channel, fn[1])
+            return
+        if os.path.getsize(fn[1]) > 0:
+            try:
+                self.__last_messages[message.author.id] = (
+                    message,
+                    await self.client.send_file(message.channel, fn[1])
+                )
+            except discord.errors.Forbidden:
+                error = 'Error: Insufficient permissions to send file files to channel #{}'.format(message.channel.name)
+                glados.log(error)
+                await self.client.send_message(message.channel, error)
+
