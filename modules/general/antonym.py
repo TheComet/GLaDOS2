6bcd5976ca85008938f97a878c1e6c040fec4605
TheComet <alex.murray@gmx.ch>
2018-01-14 04:38:25 +0100

    Fixing the bugs

--- modules/general/antonym.py
+++ modules/general/antonym.py
@@ -8,16 +8,20 @@ MAX_RESULTS = 20
 
 class Antonym(Module):
     @Module.command("antonym", "<word>", "Look up the antonym (opposite) of a word.")
-    def lookup_antonym(self, message, content):
-        first_word = content.split(num=2)[0]
+    async def lookup_antonym(self, message, content):
+        first_word = content.split(maxsplit=2)[0]
         url = URI + quote(first_word)
         response = urlopen(url).read().decode("utf-8")
         soup = BeautifulSoup(response, 'lxml')
         results = soup.find("div", {"class": "boxResult"}).find_all("a")
         if results is None:
             return await self.client.send_message(message.channel, "No results found for `{}`".format(first_word))
-        results_str = ", ".join([a.string for a in results[MAX_RESULTS:]])
-        if len(results) > MAX_RESULTS:
+        results_str = results[0].string
+        i = 1
+        while i < len(results) and len(results_str) < 950:  # Discord max length is 1000
+            results_str += ", " + results[i].string
+            i += 1
+        if len(results) > i:
             results_str += ", and more..."
 
         await self.client.send_message(message.channel, "Antonyms for \"{}\": ```{}```".format(first_word, results_str))
