6740afe2034a06e33449fd4ef51d5ba5ae762a95
TheComet <alex.murray@gmx.ch>
2018-02-13 19:49:23 +0100

    Pulling a VB6 in .del

--- modules/bot/del.py
+++ modules/bot/del.py
@@ -4,8 +4,11 @@ import glados
 class Del(glados.Module):
     @glados.Module.command('del', '', 'Deletes the most recent bot message')
     async def del_most_recent(self, message, content):
-        for msg in reversed(self.client.messages):
-            if msg.author.id == self.client.user.id:
-                await self.client.delete_message(msg)
-                break
-        await self.client.delete_message(message)
+        try:
+            for msg in reversed(self.client.messages):
+                if msg.author.id == self.client.user.id:
+                    await self.client.delete_message(msg)
+                    break
+            await self.client.delete_message(message)
+        except:
+            pass
