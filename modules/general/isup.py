e0d53d93a06a2d37832758927f1dba3d26b33254
markypooch <marcushansen89@yahoo.com>
2019-09-20 13:24:31 -0400

    Update isup.py
    compiling regex at module level

--- modules/general/isup.py
+++ modules/general/isup.py
@@ -8,6 +8,8 @@ import re
 import socket
 import urllib.request
 
+allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+
 # https://stackoverflow.com/questions/2532053/validate-a-hostname-string
 def is_valid_hostname(hostname):
     try:
@@ -15,7 +17,6 @@ def is_valid_hostname(hostname):
             return False
         if hostname[-1] == ".":
             hostname = hostname[:-1] # strip exactly one dot from the right, if present
-        allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
         return all(allowed.match(x) for x in hostname.split("."))
     except Exception as exception:
         return False
